3f9f6b285359f61de0ed6f0a082321a940b290c0
Ying <cy240705@gmail.com>
2021-04-26 23:47:01 -0700

    Helpers, Layouts, and Partials

---
+++ W7D2/solution 6/app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+    default from: 'from@example.com'
+    def welcome_email(user)
+    @user = user
+    @url = 'http://localhost:3000/session/new'
+    mail(to: @user.username, subject:"Welcome")
+  end
+end
